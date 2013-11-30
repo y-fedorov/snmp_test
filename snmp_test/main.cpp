b4d09b957254fb01d31bb0f3f98da8b37a18ec67
Yaroslav Fedorov <Fedorov.mail@gmail.com>
2013-11-30 11:27:06 +0400

    Initial Commit

---
+++ snmp_test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  snmp_test
+//
+//  Created by Yaroslav Fedorov on 30.11.13.
+//  Copyright (c) 2013 Yaroslav Fedorov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
